fb6c60a1f84eb597dbbc89ab91ef4d7744e21661
Linkazen <jpch2k4@gmail.com>
2021-09-11 19:31:42 +0100

    Just getting files with basic shell on to github.

---
+++ script.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function book(title, author, length, read) {
+    this.title = title
+    this.author = author
+    this.length = length
+    this.read = read
+
+}
+
+function addBookToLibrary() {
+
+}
+
