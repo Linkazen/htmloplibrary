bf859c189ef52de066b04148ca9f14108f179688
Linkazen <jpch2k4@gmail.com>
2021-09-14 17:14:27 +0100

    added 'add book to library array' function

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ function book(title, author, length, read) {
     this.author = author
     this.length = length
     this.read = read
-
 }
 
 function addBookToLibrary() {
-
+    let title = prompt("enter your info pp head", "title")
+    let author = prompt("enter your info pp head", "author")
+    let length = prompt("enter your info pp head", "length")
+    let read = confirm("have you read this?")
+    let addedBook = new book(title, author, length, read)
+    myLibrary.push(addedBook)
 }
+
+addBookToLibrary()
+console.log(myLibrary)
 
