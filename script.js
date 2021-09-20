43d16bb9db4cf94e14ac893ef3786edd6672aec7
Linkazen <jpch2k4@gmail.com>
2021-09-20 16:26:05 +0100

    added cards now appearing after pressing submit on form

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function addBookToLibrary() {
     for(let i = 0; i < (form.length - 2); i++) {
         bookInfo.push(form[i].value)
     }
+    console.log(bookInfo[3])
     let addedBook = new book(bookInfo[0], bookInfo[1], bookInfo[2], bookInfo[3]);
     myLibrary.push(addedBook);
     return
@@ -27,8 +28,18 @@ function addBookCard() {
     booksarea.innerHTML = "";
     for (let i = 0; i < myLibrary.length; i++) {
         let book = document.createElement("div");
-        book.className = "books"
-        book.innerText = "testing"
+        let title = document.createElement("p")
+        let author = document.createElement("p")
+        let length = document.createElement("p")
+        let read = document.createElement("p")
+        title.innerText = myLibrary[i].title
+        author.innerText = myLibrary[i].author
+        length.innerText = myLibrary[i].length
+        read.innerText = myLibrary[i].read
+        book.appendChild(title)
+        book.appendChild(author)
+        book.appendChild(length)
+        book.appendChild(read)
         booksarea.appendChild(book);
     }
 }
@@ -41,8 +52,10 @@ confirmBtn.addEventListener("click", function(){
     addBookToLibrary()
     addBookCard()
     promptBox.style.display = "none"
-},)
+    form.reset()
+})
 
 cancelBtn.addEventListener("click", function(){
     promptBox.style.display = "none"
+    form.reset()
 })
