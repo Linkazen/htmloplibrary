bc15b38c1749cd1bd2648c64be29662572e8b554
Linkazen <jpch2k4@gmail.com>
2021-09-20 14:11:33 +0100

    Added working form into array and submit/cancel buttons

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ let myLibrary = [];
 let booksarea = document.querySelector(".booksarea")
 let bookbutton = document.querySelector(".addbook")
 let promptBox = document.querySelector(".promptArea")
+let confirmBtn = document.querySelector("#confirmBtn")
+let cancelBtn = document.querySelector("#cancelBtn")
+let form = document.querySelector("#promptForm")
 
 function book(title, author, length, read) {
     this.title = title
@@ -11,13 +14,14 @@ function book(title, author, length, read) {
 }
 
 function addBookToLibrary() {
-    let title = prompt("enter your info pp head", "title");
-    let author = prompt("enter your info pp head", "author");
-    let length = prompt("enter your info pp head", "length");
-    let read = confirm("have you read this?");
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let length = document.querySelector("#length").value;
+    let read = document.querySelector("#read").checked;
     read = read ? "yes" : "no";
     let addedBook = new book(title, author, length, read);
     myLibrary.push(addedBook);
+    console.log(myLibrary)
 }
 
 function addBookCard() {
@@ -29,4 +33,15 @@ function addBookCard() {
 
 bookbutton.addEventListener("click", function(){
     promptBox.style.display = "flex"
+})
+
+confirmBtn.addEventListener("click", function(){
+    addBookToLibrary()
+    promptBox.style.display = "none"
+    form.reset();
+})
+
+cancelBtn.addEventListener("click", function(){
+    promptBox.style.display = "none"
+    form.reset()
 })
